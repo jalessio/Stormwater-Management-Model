4e33d2a45a85d26621c0ab296277900c59efcf17
barrc <barr.colleen@epa.gov>
2016-05-19 12:07:20 -0700

    .exe set up to take command line args

--- classes/main.cpp
+++ classes/main.cpp
@@ -1,28 +1,37 @@
+#include "text.h"
 #include "SWMMRunner.h"
+
+extern void writecon(char *s);
 
 int main(int argc, char* argv[])
 {
 	// note that the MAXFNAME defined in constants.h as 259
+	char blank[] = "";
 
-	char* pathinp = "c:\\users\\cbarr02\\desktop\\github\\swmm\\stormwater-management-model\\examples\\spz2_externalrainfall.inp";
-	char* pathrpt = "c:\\users\\cbarr02\\desktop\\github\\swmm\\stormwater-management-model\\examples\\spz2_externalrainfall.rpt";
-	char* pathout = "c:\\users\\cbarr02\\desktop\\github\\swmm\\stormwater-management-model\\examples\\spz2_externalrainfall.out";
-	char* pathrain = "c:\\users\\cbarr02\\desktop\\github\\swmm\\stormwater-management-model\\examples\\rainfall_for_SPZ2.dat";
+	char* pathinp = blank;
+	char* pathrpt = blank;
+	char* pathout = blank;
+	char* pathrain = blank;
 
-	char* pathinp2 = "c:\\users\\cbarr02\\desktop\\github\\swmm\\stormwater-management-model\\examples\\spz2.inp";
-	char* pathrpt2 = "c:\\users\\cbarr02\\desktop\\github\\swmm\\stormwater-management-model\\examples\\spz2.rpt";
-	char* pathout2 = "c:\\users\\cbarr02\\desktop\\github\\swmm\\stormwater-management-model\\examples\\spz2.out";
+	int err;
 
+	if (argc < 3) writecon(FMT01);
+	else
+	{
+		// --- extract file names from command line arguments
+		pathinp = argv[1];
+		pathrpt = argv[2];
+		if (argc > 3) pathout = argv[3];
+		else          pathout = blank;
+		if (argc > 4) pathrain = argv[4];
+		else		  pathrain = blank;
+		writecon(FMT02);
+	}
 
 	SWMMRunner swmmrunner(pathinp, pathrpt, pathout, pathrain);
-	int err = swmmrunner.Run();
+	err = swmmrunner.Run();
 	if (err)
 		return 1;
-
-	SWMMRunner swmmrunner2(pathinp2, pathrpt2, pathout2);
-	int err2 = swmmrunner2.Run();
-	if (err2)
-		return 2;
 
 	return 0;
 }
